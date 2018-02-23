4b45e460d9079101dbf2b13790cbe59bb5c650db
COLE CARROLL <colecarroll2@gmail.com>
2018-02-23 14:52:54 -0700

    completed queue

--- queue.js
+++ queue.js
@@ -14,7 +14,26 @@ class Queue {
 
 const q = new Queue();
 
-q.add("first in line");
-q.add("second in line");
-q.add("third in line");
-q.remove();
+class Queue {
+  constructor() {
+    this.data = [];
+  }
+
+  add(record) {
+    this.data.unshift(record);
+  }
+
+  remove() {
+    return this.data.pop();
+  }
+}
+
+const q = new Queue();
+
+q.add(1);
+q.add(2);
+q.add(3);
+let removed = q.remove();
+
+console.log(q);
+console.log(removed);
