260fd94d8fc8b9f1677a7f9fc8de83b1473b48a8
COLE CARROLL <colecarroll2@gmail.com>
2018-02-23 15:26:48 -0700

    weaving two queues into a third queue

---
+++ weavingTwoQueues.js
@@ -0,0 +1,41 @@
+class Queue {
+  constructor() {
+    this.data = [];
+  }
+
+  add(record) {
+    this.data.unshift(record);
+  }
+
+  remove() {
+    return this.data.pop();
+  }
+
+  peek() {
+    return this.data[this.data.length - 1];
+  }
+}
+
+const sourceOne = new Queue();
+const sourceTwo = new Queue();
+
+sourceOne.add(1);
+sourceTwo.add(2);
+sourceOne.add(3);
+sourceTwo.add(4);
+sourceOne.add(5);
+sourceTwo.add(6);
+
+function weave(sourceOne, sourceTwo) {
+  const q = new Queue();
+  while (sourceOne.peek() || sourceTwo.peek()) {
+    if (sourceOne.peek()) {
+      q.add(sourceOne.remove());
+    }
+
+    if (sourceTwo.peek()) {
+      q.add(sourceTwo.remove());
+    }
+  }
+  return q;
+}
