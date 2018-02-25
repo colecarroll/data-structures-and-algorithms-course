ce2730416b4ddf3aa045f92c482b87c1fa4e422a
COLE CARROLL <colecarroll2@gmail.com>
2018-02-25 12:53:44 -0700

    beginning linked list

---
+++ linked-list.js
@@ -0,0 +1,17 @@
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    const node = new Node(data, this.head);
+    this.head = node;
+  }
+}
