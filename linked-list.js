0e9b6707841b1b5349283926f74eec8ff4308f9d
COLE CARROLL <colecarroll2@gmail.com>
2018-02-26 13:22:16 -0700

    all methods defined for linked list

--- linked-list.js
+++ linked-list.js
@@ -53,4 +53,96 @@ class LinkedList {
     }
     this.head = this.head.next;
   }
+
+  removeLast() {
+    if (!this.head) {
+      return;
+    }
+
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+
+    let previous = this.head;
+    let node = this.head.next;
+    while (node.next) {
+      previous = node;
+      node = node.next;
+    }
+    previous.next = null;
+  }
+
+  insertLast(data) {
+    const last = this.getLast();
+    if (last) {
+      last.next = new Node(data);
+    } else {
+      this.head = new Node(data);
+    }
+  }
+
+  getAt(index) {
+    let node = this.head;
+    let counter = 0;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+      counter++;
+      node = node.next;
+    }
+    return null;
+  }
+
+  removeAt(index) {
+    if (!this.head) {
+      return;
+    }
+    if (index === 0) {
+      this.head = this.head.next;
+      return;
+    }
+    const previous = this.getAt(index - 1);
+    if (!previous || !previous.next) {
+      return;
+    }
+
+    previous.next = previous.next.next;
+  }
+
+  insertAt(data, index) {
+    //empty list
+    if (!this.head) {
+      this.head = new Node(data);
+      return;
+    }
+    //inserting at first position
+    if (index === 0) {
+      this.head = new Node(data, this.head);
+      return;
+    }
+    //inserting at any other position
+    const previous = this.getAt(index - 1) || this.getLast(); //if this.getAt(index-1) returns falsy, then index out of bounds, insert at end of list
+    const node = new Node(data, previous.next);
+    previous.next = node;
+  }
+
+  forEach(fn) {
+    let node = this.head;
+    let counter = 0;
+    while (node) {
+      fn(node, counter);
+      node = node.next;
+      counter++;
+    }
+  }
+
+  *[Symbol.iterator]() {
+    let node = this.head;
+    while (node) {
+      yield node;
+      node = node.next;
+    }
+  }
 }
